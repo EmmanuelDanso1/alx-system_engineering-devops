857e5788d38ca753930cc4950c61a1bb754cbd05
emmanueldanso1 <kwekudanso21@gmail.com>
2024-06-28 15:01:10 -0700

    creating files

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# creats file using ensure
+file {'/tmp/school':
+	ensure => file,
+		mode => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+	content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,1 @@
+This is a readme which emphasizes on DevOps and using configuration management to make files in CI/CD
